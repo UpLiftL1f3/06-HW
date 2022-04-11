532c8e625e356138fd5e164f8721bcce17456aef
KnightNight <gilbenvenitz18@gmail.com>
2022-04-10 22:01:18 -0500

    initial upload

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+// GIVEN a weather dashboard with form inputs
+// When  i search for a city
+// THEN present current and future weather conditions for that city &&&&& add that city to search history
+// WHEN i view current weather conditions for that city
+// THEN i am presented with the city name/date/an icon representing the current weather condition/the temp/humidity/wind speed/UV index
+// When i view the UV index
+// THEN i am presented with a color that indicates whether the conditions are favorable/moderate/severe
+// WHEN i view future conditions for that city
+// THEN i am presented with a 5 day forecast that displays the date/an icon representing weather conditions/temp/wind speed/and humidity for those 5 days
+// WHEN i click on a city in the search history
+// THEN i am again presented with current and future conditions for that city
+
+// VARIABLES
+let userCity = document.querySelector('#search-input').value;
+let searchBtn = $('#search-btn');
+let searchHistoryContainer = $('#search-history-container');
+let searchHistoryArr = [];
+let apiKey = 'f44b46adae4f2a1382ec3ffdb78f40c8';
+// right-side-body variables
+// top
+let rightTopHeader = document.getElementById('top-right-header');
+
+// FUNCTION Declarations
+function onSearch(e) {
+  e.preventDefault();
+  console.log(userCity);
+  if (userCity.length === 0) {
+    return (userCity = 'Austin');
+  }
+  getCityInfo(userCity, apiKey);
+  // searchHistory(userCity);
+  return userCity;
+}
+
+// Not working correctly Need to find a way select container's children (keep getting Undefined)
+function searchHistory(city) {
+  searchHistoryArr.unshift(city);
+  if (searchHistoryArr.length > 5) {
+    searchHistoryArr.pop();
+  }
+  for (let i = 0; i < searchHistoryArr.length; i++) {
+    let newBtn = document.createElement('button');
+    newBtn.setAttribute('class', 'btn btn-secondary w-100 m-2');
+    newBtn.textContent = searchHistoryArr[0];
+    searchHistoryContainer.append(newBtn);
+  }
+  console.log(searchHistoryArr);
+}
+
+// main purpose of this function is to get Longitude and Latitude
+function getCityInfo(city, key) {
+  let apiUrl =
+    'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + key;
+  console.log(apiUrl);
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          // let Latitude = this.lat;
+          // let Longitude = this.lon;
+          // let latLonArr = [Latitude, Longitude];
+          // console.log(latLonArr);
+          // return latLonArr;
+        });
+      } else {
+        alert('Error: ' + response.statusText); // if response was not okay then its sending an Alert (which is bad bc its a blocker) with the response status displayed
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to weatherAPI'); //is there is a server side error then an Alert (which is bad bc its a blocker) with the response will be displayed
+    });
+}
+
+// functional CODE
+searchBtn.on('click', onSearch);
+
+// top-right-header
+rightTopHeader.textContent = userCity;
+console.log(userCity);
